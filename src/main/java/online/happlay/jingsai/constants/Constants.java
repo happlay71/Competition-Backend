2ef0499a4669b4889a9d7e6d71c9586dba246aed
quyang <1633433173@qq.com>
2025-01-03 11:02:02 +0800

    完善token令牌的过期机制

---
+++ src/main/java/online/happlay/jingsai/constants/Constants.java
@@ -0,0 +1,8 @@
+package online.happlay.jingsai.constants;
+
+public class Constants {
+
+    public static final Long EXPIRATION_TIME = 60 * 60 * 24L;
+
+    public static final String SECRET_KEY = "happlay";
+}
--- src/main/java/online/happlay/jingsai/utils/JwtUtils.java
+++ src/main/java/online/happlay/jingsai/utils/JwtUtils.java
@@ -1,8 +1,10 @@
 package online.happlay.jingsai.utils;
 
 import cn.hutool.core.util.StrUtil;
-import cn.hutool.jwt.JWT;
 import cn.hutool.jwt.signers.JWTSignerUtil;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import online.happlay.jingsai.common.ErrorCode;
 import online.happlay.jingsai.exception.BusinessException;
 import online.happlay.jingsai.exception.ThrowUtils;
@@ -11,6 +13,11 @@ import online.happlay.jingsai.model.vo.LoginUserVO;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
+
+import java.util.Date;
+
+import static online.happlay.jingsai.constants.Constants.EXPIRATION_TIME;
+import static online.happlay.jingsai.constants.Constants.SECRET_KEY;
 
 @Component
 public class JwtUtils {
@@ -23,30 +30,65 @@ public class JwtUtils {
      * @return Token
      */
     public String createToken(String userId, String userRole) {
+        // 设置过期时间
+        long expirationTime = System.currentTimeMillis() + EXPIRATION_TIME;
+        Date expirationDate = new Date(expirationTime);
+
+        // 创建并签名JWT
         return JWT.create()
-                .setPayload("userId", userId)
-                .setPayload("role", userRole)
-                .setSigner(JWTSignerUtil.none())
-                .sign();
+                .withClaim("userId", userId)
+                .withClaim("role", userRole)
+                .withExpiresAt(expirationDate)  // 设置过期时间
+                .sign(Algorithm.HMAC256(SECRET_KEY));
     }
 
+    /**
+     * 获取用户ID
+     *
+     * @param request HttpServletRequest
+     * @return userId
+     */
     public String getUserId(HttpServletRequest request) {
         String token = request.getHeader("token");
-        verifyToken(token);
-        JWT jwt = JWT.of(token);
-        return (String) jwt.getPayload("userId");
+        verifyToken(token); // 验证token有效性
+        DecodedJWT jwt = JWT.decode(token);
+        return jwt.getClaim("userId").asString();
     }
 
+    /**
+     * 获取用户角色
+     *
+     * @param request HttpServletRequest
+     * @return userRole
+     */
     public String getUserRole(HttpServletRequest request) {
         String token = request.getHeader("token");
-        verifyToken(token);
-        JWT jwt = JWT.of(token);
-        return (String) jwt.getPayload("role");
+        verifyToken(token); // 验证token有效性
+        DecodedJWT jwt = JWT.decode(token);
+        return jwt.getClaim("role").asString();
     }
 
+    /**
+     * 验证Token的有效性
+     *
+     * @param token JWT Token
+     */
     public void verifyToken(String token) {
-        boolean verify = JWT.of(token).verify();
-        ThrowUtils.throwIf(!verify, ErrorCode.TOKEN_ERROR);
+        try {
+            // 解码JWT并验证签名
+            DecodedJWT jwt = JWT.decode(token);
+
+            // 验证Token是否过期
+            Date expiration = jwt.getExpiresAt();
+            if (expiration.before(new Date())) {
+                throw new RuntimeException("Token过期");
+            }
+
+            // 如果需要更严格的验证签名，可以使用JWT.require(Algorithm.HMAC256(SECRET_KEY))来进一步验证签名。
+
+        } catch (Exception e) {
+            throw new RuntimeException("Invalid token: " + e.getMessage());
+        }
     }
 
 }
